2388138903680c9f1a872d53c634f11e10d2b1d0
Villena Guillaume <guiguivil@gmail.com>
2019-11-16 19:38:30 +0100

    App port configurable using environment variable

--- config.js
+++ config.js
@@ -2,11 +2,7 @@ const path = require('path');
 
 const config = {
   ROOT_DIR: __dirname,
-  URL_PORT: 3000,
-  URL_PATH: 'http://localhost',
-  BASE_VERSION: 'v1',
-  CONTROLLER_DIRECTORY: path.join(__dirname, 'controllers'),
+  URL_PORT: process.env.PORT || 3000,
 };
 config.OPENAPI_YAML = path.join(config.ROOT_DIR, 'api', 'openapi.yaml');
-config.FULL_PATH = `${config.URL_PATH}:${config.URL_PORT}/${config.BASE_VERSION}`;
 module.exports = config;
